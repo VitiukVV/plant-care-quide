5695014695bf83d74538379a880641511e062fcb
VolodymyrVitiuk <v.v.endeavour@gmail.com>
2024-03-20 15:04:32 +0200

    Update main.tsx
    fix BrowserRouter basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/plant-care-quide">
+    <BrowserRouter basename="/plant-care-quide/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
