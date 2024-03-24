807e42865272e74dacf8ea9f8209e41953819c1d
Irina Mishchenko <i.mishchenko95@gmail.com>
2024-03-24 16:38:18 +0200

    PCG-15: added logo, text to footer, fixed styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,10 +5,29 @@ import {
   Toolbar,
   BottomNavigation,
   BottomNavigationAction,
+  Typography,
 } from '@mui/material';
+import Logo from '../../icons/Logo';
 
 const Footer = () => {
   const [value, setValue] = useState<number>(0);
+  const footerText: string =
+    'Your virtual garden assistant. Add your favourite plants, identify it with photo, find care tips and get reminders for watering and other useful events.';
+
+  const bottomNavigationActionStyles = {
+    '& .MuiBottomNavigationAction-label': {
+      color: '#fff',
+      fontSize: '20px',
+      borderBottom: '2px solid transparent',
+      paddingBottom: '5px',
+    },
+    '& .MuiBottomNavigationAction-label.Mui-selected': {
+      color: '#fff',
+      fontSize: '20px',
+      borderBottom: '2px solid #fff',
+      paddingBottom: '5px',
+    },
+  };
 
   return (
     <Box
@@ -16,7 +35,6 @@ const Footer = () => {
       sx={{
         position: 'fixed',
         bottom: '0',
-        height: '100px',
         width: '100%',
         bgcolor: '#56817A',
       }}
@@ -27,12 +45,21 @@ const Footer = () => {
           maxWidth: '1320px',
           margin: '0 auto',
           padding: '10px 0',
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexWrap: 'wrap',
         }}
       >
         <BottomNavigation
           showLabels
           value={value}
-          sx={{ display: 'flex', bgcolor: 'inherit' }}
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '10px',
+            bgcolor: 'inherit',
+            maxWidth: '30%',
+          }}
           onChange={(_event: ChangeEvent<{}>, newValue: number) => {
             setValue(newValue);
           }}
@@ -41,35 +68,21 @@ const Footer = () => {
             label="Home"
             component={NavLink}
             to="/"
-            sx={{
-              '& .MuiBottomNavigationAction-label': {
-                color: '#fff',
-                fontSize: '25px',
-                borderBottom: '2px solid transparent',
-                paddingBottom: '5px',
-              },
-              '& .Mui-selected': {
-                borderBottom: '2px solid #fff',
-              },
-            }}
+            sx={bottomNavigationActionStyles}
           />
           <BottomNavigationAction
             label="Garden"
             component={NavLink}
             to="/garden"
-            sx={{
-              '& .MuiBottomNavigationAction-label': {
-                color: '#fff',
-                fontSize: '25px',
-                borderBottom: '2px solid transparent',
-                paddingBottom: '5px',
-              },
-              '& .Mui-selected': {
-                borderBottom: '2px solid #fff',
-              },
-            }}
+            sx={bottomNavigationActionStyles}
           />
         </BottomNavigation>
+        <Typography
+          sx={{ textAlign: 'center', color: '#fff', maxWidth: '60%' }}
+        >
+          {footerText}
+        </Typography>
+        <Logo />
       </Toolbar>
     </Box>
   );
